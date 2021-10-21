ea9a4ecefb12bdeda03aa04d7456a9fbd0a08724
Alfredo Esteban <alfredo.estmiranda@gmail.com>
2021-10-21 15:05:22 -0500

    funtion to calculate if a number is perfect was added

---
+++ index.js
@@ -0,0 +1,13 @@
+function perfectNumber (n) {
+  let x = 0
+  for(let i = 1; i <= n / 2; ++i ){
+    if(n % i === 0) {
+      x += i
+    }
+  }
+    const result = x != 0 && x == n
+  
+    result ?  console.log(`El numero ${n} es perfecto`) : console.log(`El numero ${n} no es perfecto`)
+}
+
+perfectNumber(4)
